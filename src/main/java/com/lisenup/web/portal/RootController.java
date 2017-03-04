07392039f71e6ccb961a3d9567776bf187b8ccb2
vmogilev <oracle.ux@gmail.com>
2017-03-03 17:29:38 -0800

    root done

---
+++ src/main/java/com/lisenup/web/portal/RootController.java
@@ -0,0 +1,18 @@
+package com.lisenup.web.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+
+	@RequestMapping(method=RequestMethod.GET)
+    public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
