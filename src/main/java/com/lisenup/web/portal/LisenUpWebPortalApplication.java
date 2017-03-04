66ae658e565bbc45062fe63e228ebb3e7798ed4a
vmogilev <oracle.ux@gmail.com>
2017-03-03 16:27:14 -0800

    initial

---
+++ src/main/java/com/lisenup/web/portal/LisenUpWebPortalApplication.java
@@ -0,0 +1,12 @@
+package com.lisenup.web.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisenUpWebPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisenUpWebPortalApplication.class, args);
+	}
+}
