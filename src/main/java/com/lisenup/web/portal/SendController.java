43fe1dc56b1cd9e1e6d9dedec55f14e36cf6f683
vmogilev <oracle.ux@gmail.com>
2017-03-06 22:25:06 -0800

    send controller now parses off root URI

--- src/main/java/com/lisenup/web/portal/SendController.java
+++ src/main/java/com/lisenup/web/portal/SendController.java
@@ -2,15 +2,17 @@ package com.lisenup.web.portal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SendController {
 
-    @RequestMapping("/send")
-    public String greeting(@RequestParam(value="to", required=true) String toName, Model model) {
-        model.addAttribute("to", toName);
+	// regex filters out stuff like PC@Bddd
+	@RequestMapping(value = "/{toId:[A-Za-z0-9\\-\\_]+}", method = RequestMethod.GET)
+    public String greeting(@PathVariable("toId") String toId, Model model) {
+        model.addAttribute("toId", toId);
         return "send";
     }
 }
