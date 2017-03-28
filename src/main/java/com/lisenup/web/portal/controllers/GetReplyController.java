4da35f4fbd99b7085930669d249af9d6ced55d68
vmogilev <oracle.ux@gmail.com>
2017-03-27 23:11:59 -0700

    cleanup imports

--- src/main/java/com/lisenup/web/portal/controllers/GetReplyController.java
+++ src/main/java/com/lisenup/web/portal/controllers/GetReplyController.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
